f8d50bf3836a5501c000b1bff22846c1ffb5a248
udaysinghranaaa <udaysinghranaaa@gmail.com>
2026-03-16 21:48:55 +0530

    err solve

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: true
+  },
   preview: {
     host: true,
     port: process.env.PORT,
-    allowedHosts: "all"
+    allowedHosts: true
   }
-})
+});
